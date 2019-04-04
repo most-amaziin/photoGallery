31fa42dd6832a9a7fcffddf321a08b3bd9921764
matthew <matt.feinberg@gmail.com>
2019-04-04 09:42:24 -0500

    changed push route to dev instead of master

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'http://ec2-3-19-73-29.us-east-2.compute.amazonaws.com/',
       key: '~/.ssh/FECPhotoGallery.pem',
-      ref: 'origin/master',
+      ref: 'origin/dev',
       repo: 'https://github.com/threetexansandacanadian/photoGallery.git',
       path: '/home/ubuntu/fecphotogallery',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
