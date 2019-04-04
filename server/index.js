0e6c0412cd3c740a914a835faab817ae3be6d34b
matthew <matt.feinberg@gmail.com>
2019-04-04 17:10:22 -0500

    more deploy tests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+app.use(express.static('dist'))
 app.listen(3000, () => console.log('Server running on port 3000'))
