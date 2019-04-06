de7a474f7a6e3e33398e204f9603777b4a709472
matthew <matt.feinberg@gmail.com>
2019-04-06 13:31:01 -0500

    changed rep in ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-3-17-26-34.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/FECPhotoGallery.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/threetexansandacanadian/photoGallery.git',
+      repo: 'git@github.com:threetexansandacanadian/photoGallery.git',
       path: '/home/ubuntu/fecphotogallery',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
