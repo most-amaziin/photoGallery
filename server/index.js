849aba6faaa3e2f105bfc013d192edc900c04618
matthew <matt.feinberg@gmail.com>
2019-04-06 11:01:10 -0500

    typo on index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // const express = require('express')
 // const app = express()
 // app.use(express.static('dist'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+// app.listen(3000, () => console.log('Server running on port 3000'))
 
 const express = require('express');
 const app = express();
