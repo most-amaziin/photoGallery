7686229bbbfe3ca149c586b3988aaa43f8820e01
matthew <matt.feinberg@gmail.com>
2019-04-08 15:40:29 -0500

    ecosystem.config.js git status ;

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps: [{
-    name: 'fecphotogallery',
-    script: './server/server.js'
-  }],
+  apps: [
+    {
+      name: "fecphotogallery",
+      script: "./server/server.js"
+    }
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-3-17-26-34.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/FECPhotoGallery.pem',
-      ref: 'origin/master',
-      repo: 'https://github.com/threetexansandacanadian/photoGallery.git',
-      path: '/home/ubuntu/fecphotogallery',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      user: "ubuntu",
+      host: "ec2-13-58-49-169.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/FECPhotoGallery.pem",
+      ref: "origin/master",
+      repo: "https://github.com/threetexansandacanadian/photoGallery.git",
+      path: "/home/ubuntu/fecphotogallery",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
-}
+};
--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import Gallery from '../presentational/gallery';
-import View from '../presentational/view';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import Gallery from "../presentational/gallery";
+import View from "../presentational/view";
 
 export default class FormContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 64,
+      productId: 78,
       productPhotos: [],
-      productName: 'product photo',
+      productName: "product photo",
       photo: {
         id: 1,
-        url: 'https://s3.amazonaws.com/fecphotogallery2019/photos/1_1.jpg',
+        url: "https://s3.amazonaws.com/fecphotogallery2019/photos/1_1.jpg",
         product_id: 1
       }
-    }
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    console.log('1ZZZ')
+    console.log("1ZZZ");
     const currentPhoto = this.state.photo.id;
     const currentProduct = this.state.productName;
-    axios.get('/photos', { params: { id: this.state.productId } })
+    axios
+      .get("/photos", { params: { id: this.state.productId } })
       .then(results => {
-        this.setState({ productPhotos: results.data, photo: results.data[0] }, () => {
-          console.log('6ZZZ')
-        })
+        this.setState(
+          { productPhotos: results.data, photo: results.data[0] },
+          () => {
+            console.log("6ZZZ");
+          }
+        );
       })
-      .catch('Could not load product photos at client gallery')
-      .then(axios.get('/product/id', { params: { id: this.state.productId } })
-        .then(result => {
-          console.log('6yyy', result.data)
-          this.setState({ productName: result.data[0].name })
-        })
-        .catch('Could not load chosen photo at client view')
-      )
+      .catch("Could not load product photos at client gallery")
+      .then(
+        axios
+          .get("/product/id", { params: { id: this.state.productId } })
+          .then(result => {
+            console.log("6yyy", result.data);
+            this.setState({ productName: result.data[0].name });
+          })
+          .catch("Could not load chosen photo at client view")
+      );
   }
 
   handleClick(e) {
@@ -48,22 +54,28 @@ export default class FormContainer extends React.Component {
         //THIS WILL UPDATE WHEN CONNECTING TO OTHER COMPONENTS
         product_id: this.state.productId
       }
-    })
+    });
   }
 
   render() {
-
     return (
       <div>
         <table>
-          <td id="gallery" class='gallery'>
-            <Gallery clickPhoto={this.handleClick} productPhotos={this.state.productPhotos} productName={this.state.productName} />
+          <td id="gallery" class="gallery">
+            <Gallery
+              clickPhoto={this.handleClick}
+              productPhotos={this.state.productPhotos}
+              productName={this.state.productName}
+            />
           </td>
-          <td id="full" class='view'>
-            <View photo={this.state.photo} productName={this.state.productName} />
+          <td id="full" class="view">
+            <View
+              photo={this.state.photo}
+              productName={this.state.productName}
+            />
           </td>
         </table>
       </div>
-    )
+    );
   }
 }
