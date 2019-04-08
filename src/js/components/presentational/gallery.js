7b03ff7cf5f788bf4171b9541355123252dd8fa4
matthew <matt.feinberg@gmail.com>
2019-04-07 19:51:39 -0500

    changes gallery class to const

--- src/js/components/presentational/gallery.js
+++ src/js/components/presentational/gallery.js
@@ -2,25 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-export default class Gallery extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const tempName = this.props.productName;
-    const photos = this.props.productPhotos.map((photo) => (
-      <tr class='photosContainer'>
-        <span class='photoSpan'></span>
-        <img class='photos' onClick={this.props.clickPhoto} id={photo.id} src={photo.url} alt={`photo ${photo.id} for product ${photo.product_id}: ${tempName}`}></img>
-        {/* <p>{this.props.productName}</p>
-        <p>text above?</p> */}
-      </tr >
-    ));
-    return (
-      <table>
-        {photos}
-      </table>
-    )
-  }
+const Gallery = (props) => {
+  const photos = props.productPhotos.map((photo) => (
+    <tr class='photosContainer'>
+      <span class='photoSpan'></span>
+      <img class='photos' onClick={props.clickPhoto} id={photo.id} src={photo.url} alt={`photo ${photo.id} for product ${photo.product_id}: ${props.productName}`}></img>
+    </tr >
+  ));
+  return (
+    <table>
+      {photos}
+    </table>
+  )
 }
+
+export default Gallery
