8223fe1ccb6de5a082c6b0c22b233b88188d1df8
AndersAustin <23081361+AndersAustin@users.noreply.github.com>
2019-04-23 15:14:47 -0500

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-18-188-249-142.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/FEC_Carousel.pem',
+        key: '~/.ssh/fec_carousel.pem',
         ref: 'origin/master',
         repo: 'https://github.com/most-amaziin/photoGallery.git',
         path: '/home/ubuntu/AAnderson_photoGallery',
